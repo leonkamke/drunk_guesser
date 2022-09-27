c9142386148800311933bdfbf5d0971ef9ab10ff
burgluca <burgis.luca@freenet.de>
2022-09-27 15:46:23 +0200

    Kategorie - Startbutton

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -24,89 +24,86 @@ class _CategoryCardState extends State<CategoryCard> {
   Widget build(BuildContext context) {
     final displayWidth = MediaQuery.of(context).size.width;
     final displayHeight = MediaQuery.of(context).size.height;
-
-    return Stack(
-      alignment: Alignment.center,
-      children: [
-        GestureDetector(
-          onTap: selectCategoryCard,
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.symmetric(
-                    vertical: 3, horizontal: displayWidth * 0.03),
-                margin: EdgeInsets.symmetric(
-                    vertical: 3, horizontal: displayWidth * 0.03),
-                height: displayHeight * 0.105,
-                color: Colors.transparent,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: 70,
-                      height: 70,
-                      padding: const EdgeInsets.all(7),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(15),
-                        color: const Color(0xFFF6F6F6),
-                        boxShadow: const [
-                          BoxShadow(
-                              color: Colors.black54,
-                              offset: Offset(0, 3),
-                              blurRadius: 6)
-                        ],
-                      ),
-                      child: Image.asset(
-                        widget.category.iconPath,
-                        height: 10,
-                        fit: BoxFit.contain,
-                      ),
+    if (widget.category.purchased) {
+      return GestureDetector(
+        onTap: selectCategoryCard,
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(
+                  vertical: 3, horizontal: displayWidth * 0.03),
+              margin: EdgeInsets.symmetric(
+                  vertical: 3, horizontal: displayWidth * 0.03),
+              height: displayHeight * 0.105,
+              color: Colors.transparent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 70,
+                    height: 70,
+                    padding: const EdgeInsets.all(7),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: const Color(0xFFF6F6F6),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black54,
+                            offset: Offset(0, 3),
+                            blurRadius: 6)
+                      ],
                     ),
-                    SizedBox(
-                      width: displayWidth * 0.05,
+                    child: Image.asset(
+                      widget.category.iconPath,
+                      height: 10,
+                      fit: BoxFit.contain,
                     ),
-                    Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            widget.category.name,
-                            style: const TextStyle(
-                              color: Color(0xFF292F38),
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
-                              fontFamily: "Quicksand",
-                            ),
+                  ),
+                  SizedBox(
+                    width: displayWidth * 0.05,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          widget.category.name,
+                          style: const TextStyle(
+                            color: Color(0xFF292F38),
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                            fontFamily: "Quicksand",
                           ),
-                          AutoSizeText(
-                            maxLines: 3,
-                            widget.category.description,
-                            style: const TextStyle(
-                              color: Color(0xFF292F38) ,
-                              fontSize: 11,
-                              // fontWeight: FontWeight.bold,
-                              fontFamily: "Quicksand",
-                            ),
+                        ),
+                        AutoSizeText(
+                          maxLines: 3,
+                          widget.category.description,
+                          style: const TextStyle(
+                            color: Color(0xFF292F38),
+                            fontSize: 11,
+                            // fontWeight: FontWeight.bold,
+                            fontFamily: "Quicksand",
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      width: displayWidth * 0.05,
-                    ),
-                    Icon(
-                      Icons.check_circle_outline_rounded,
-                      size: 30,
-                      color: selected
-                          ? const Color(0xff1e9d00)
-                          : const Color(0x1affffff),
-                    ),
-                  ],
-                ),
+                  ),
+                  SizedBox(
+                    width: displayWidth * 0.05,
+                  ),
+                  Icon(
+                    Icons.check_circle_outline_rounded,
+                    size: 30,
+                    color: selected
+                        ? const Color(0xff1e9d00)
+                        : const Color(0x1affffff),
+                  ),
+                ],
               ),
-              /*
+            ),
+            /*
               Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(15),
@@ -115,12 +112,100 @@ class _CategoryCardState extends State<CategoryCard> {
                 height: 2,
                 width: displayWidth * 0.9,
               ),*/
-            ],
-          ),
+          ],
         ),
-        getLockContainer(),
-      ],
-    );
+        //getLockContainer(),
+      );
+    } else {
+      return Stack(
+        children: [
+          GestureDetector(
+            onTap: selectCategoryCard,
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 3, horizontal: displayWidth * 0.03),
+                  margin: EdgeInsets.symmetric(
+                      vertical: 3, horizontal: displayWidth * 0.03),
+                  height: displayHeight * 0.105,
+                  color: Colors.transparent,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 70,
+                        height: 70,
+                        padding: const EdgeInsets.all(7),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          color: const Color(0xFFF6F6F6),
+                          boxShadow: const [
+                            BoxShadow(
+                                color: Colors.black54,
+                                offset: Offset(0, 3),
+                                blurRadius: 6)
+                          ],
+                        ),
+                        child: Image.asset(
+                          widget.category.iconPath,
+                          height: 10,
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                      SizedBox(
+                        width: displayWidth * 0.05,
+                      ),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              widget.category.name,
+                              style: const TextStyle(
+                                color: Color(0xFF292F38),
+                                fontSize: 17,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: "Quicksand",
+                              ),
+                            ),
+                            AutoSizeText(
+                              maxLines: 3,
+                              widget.category.description,
+                              style: const TextStyle(
+                                color: Color(0xFF292F38),
+                                fontSize: 11,
+                                // fontWeight: FontWeight.bold,
+                                fontFamily: "Quicksand",
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        width: displayWidth * 0.05,
+                      ),
+                    ],
+                  ),
+                ),
+                /*
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: const Color(0xd2292f38),
+                ),
+                height: 2,
+                width: displayWidth * 0.9,
+              ),*/
+              ],
+            ),
+          ),
+          getLockContainer(),
+        ],
+      );
+    }
   }
 
   Widget getLockContainer() {
@@ -138,11 +223,11 @@ class _CategoryCardState extends State<CategoryCard> {
           margin: EdgeInsets.symmetric(
               vertical: 3, horizontal: displayWidth * 0.03),
           height: displayHeight * 0.105,
-          width: displayWidth * 0.9,
-          decoration: BoxDecoration(
+          width: displayWidth * 1,
+          /*decoration: BoxDecoration(
             color: Color(0x96292F38),
             borderRadius: BorderRadius.circular(15),
-          ),
+          ),*/
           child: const Icon(
             Icons.lock_outline_rounded,
             size: 30,
