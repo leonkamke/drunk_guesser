f9799da1794e751de36da15ac40a3854ebbd152b
burgluca <burgis.luca@freenet.de>
2022-09-27 22:33:36 +0200

    app colors test

--- lib/models/app_colors.dart
+++ lib/models/app_colors.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 class AppColors {
+  /*
   //Background
   static const Color background_1 = Color(0xFFFFFBDF);
   static const Color background_2 = Color(0xFFFFF7B2);
@@ -17,5 +18,22 @@ class AppColors {
   static const Color schriftFarbe_category = Color(0xff34656d);
   static const Color iconBackground = Color(0xffffffff);
   static const Color categoryDivider = Color(0xff34656d);
+*/
+  //Background
+  static const Color background_1 = Color(0xFF80A5D7);
+  static const Color background_2 = Color(0xFFA0C2F1);
+  //appbar
+  static const Color appbarBackground = Color(0xff06283d);
+  //Homescreen
+  static const Color buttonHomeScreenBackground = Color(0xffdff6ff);
+  static const Color buttonHomeScreenSchrift = Color(0xff06283d);
+  static const Color drunkguesserSchrift = Color(0xffdff6ff);
+  //schrift
+  static const Color schriftFarbe_hell = Color(0xffdff6ff);
+  static const Color schriftFarbe_dunkel = Color(0xff06283d);
+  //Category
+  static const Color schriftFarbe_category = Color(0xff06283d);
+  static const Color iconBackground = Color(0xffdff6ff);
+  static const Color categoryDivider = Color(0xff06283d);
 
 }
