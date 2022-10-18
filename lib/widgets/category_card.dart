2dcec523c3d0ed9e40ce5f7e426938eb9f9a5d31
burgluca <burgis.luca@freenet.de>
2022-10-18 15:21:22 +0200

    lock size

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -252,8 +252,8 @@ class _CategoryCardState extends State<CategoryCard> {
           width: displayWidth * 1,
           alignment: Alignment.center,
           child: SizedBox(
-            width: displayHeight * 0.057,
-            height: displayHeight * 0.057,
+            width: displayHeight * 0.045,
+            height: displayHeight * 0.045,
             child: Image.asset(
               "assets/icons/lock_icon.png",
               fit: BoxFit.contain,
