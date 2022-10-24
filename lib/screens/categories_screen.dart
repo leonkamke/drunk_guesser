d707809e6cff57dc90ab11c97d23785b3b8f6d2e
burgluca <burgis.luca@freenet.de>
2022-10-24 21:35:25 +0200

    fix divider

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -139,7 +139,7 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
                         return Divider(
                           color: AppColors.categoryDivider,
                           thickness: 1.5,
-                          height: displayHeight*0.04,
+                          height: 32,
                           indent: displayWidth * 0.05,
                           endIndent: displayWidth * 0.05,
                         );
